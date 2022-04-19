86a1f6f37c84de0aa0d8bdaf23ea19b103e16a90
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-04-20 01:51:10 +0200

    Add files via upload

--- 2022-Q2-R-1 [cor, pairs, corrplot]/src/2022-Q2-R-1 [cor, pairs, corrplot]/MyScript5.R
+++ 2022-Q2-R-1 [cor, pairs, corrplot]/src/2022-Q2-R-1 [cor, pairs, corrplot]/MyScript5.R
@@ -1,28 +1,62 @@
-x = c(17,19,18,20,18)
-y = c(28,30,27,29,28)
+# wprowadzenie wartości zmiennych x1 oraz y1
+x1 = c(17,20,16,18,18)
+y1 = c(19,22,21,21,18)
 
-# obliczanie korelacji Pearsona
-cor(x,y, method = c("spearman"))
+# obliczanie wartości średniej, itd. dla wartości x1
+summary(x1)
+# obliczenie wyłącznie wartości średniej (mean)
+mean(x1)
+# obliczenie wariancji dla x1
+var(x1)
 
+# obliczanie wartości średniej, itd. dla wartości y1
+summary(y1)
+# obliczenie wyłącznie wartości średniej (mean)
+mean(y1)
+# obliczenie wariancji dla y1
+var(y1)
+
+# obliczenie współczynnika korelacji, jaka wychodzi pomiędzy 2 zmiennymi (x1, y1)
+cor(x1,y1)
+
+# obliczenie współcznniku kowariancji Pearsona pomiędzy dwiema zmiennymi (x1, y1)
+cov(x1,y1, method = c("pearson"))
+
+# obliczanie współczynniku korelacji Spearmana pomiędzy dwiema zmiennymi [x1 oraz y1]
+cor(x1,y1, method = c("spearman"))
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# wyświetlenie instrukcji dla cor
 ??cor
 help(cor)
 
-xx = c(14,15,13,14,7,14,14)
-yy = c(44,44,38,45,48,50,46)
+# wprowadzenie wartości zmiennych x2 oraz y2
+x2 = c(9,16,11,12,11,9,14,9,10,9)
+y2 = c(42,49,46,44,49,45,50,48,52,54)
 
-cor(xx,yy, method = c("kendall"))
+# obliczenie współczynnika korelacji, jaka wychodzi pomiędzy 2 zmiennymi (x2, y2)
+cor(x2,y2)
+
+# obliczenie współcznniku kowariancji Pearsona pomiędzy dwiema zmiennymi (x2, y2)
+cov(x2,y2, method = c("pearson"))
+
+# obliczanie współczynniku korelacji Spearmana pomiędzy dwiema zmiennymi [x2 oraz y2]
+cor(x2,y2, method = c("spearman"))
+
+# obliczenie współczynnika korelacji Kendalla dla zmiennych x2 i y2
+cor(x2,y2, method = c("kendall"))
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 # import danych z pliku .csv
-
 read.csv(file = "US_energy_consumption.csv", header = TRUE, sep = ";", dec = ",")
 
-
-
+# import danych z pliku .csv [ oraz nadanie nazwy dane dla danych z pliku .csv ]
 dane = read.csv(file = "US_energy_consumption.csv", header = TRUE, sep = ";", dec = ",")
 head (dane)
 
 # okrojenie tabelki z nazw (z pierwszej kolumnmy)
-
 x = dane[-1,-1]
 x
 
@@ -30,8 +64,9 @@ cor(x)
 
 pairs(x)
 
+# instalacja paczki corrplot
 install.packages("corrplot")
 library(corrplot)
 
-
+# użycie zainstalowanej paczki corrplot
 corrplot(cor(x))
