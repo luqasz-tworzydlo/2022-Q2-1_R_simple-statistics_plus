80210ffd4acdb998cf6d19678c0a15d0df18c917
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-05-18 17:23:28 +0200

    Add files via upload

---
+++ 2022-Q2-R-2 [pt, summary, var]/src/2022-Q2-R-2 [pt, summary, var]/MyScriptMS.R
@@ -0,0 +1,25 @@
+# /// /// /// /// /// /// ///
+# test nr 1
+pt(0,22)
+# test nr 2
+pt(1.97,22)
+# test nr 3
+pt(-1.04,23)
+# /// /// /// /// /// /// ///
+# obliczenie alfy
+pt(0,19)
+# próba obliczeniowa
+pt(3.29509887,19)
+# /// /// /// /// /// /// ///
+# nadanie wartości zmiennych dla x
+x = c (0.3, 2.37, 1.15, 0.51, 1.55,
+       2.22, -0.17, 1.03, -0.27, 0.84,
+       0.08, 0.72, 1, 0.79, 0.44,
+       0.42, -0.37, -1.14, -0.14, 1.82)
+# obliczanie wartości średniej, itd. dla wartości x
+summary(x)
+# obliczenie wyłącznie wartości średniej (mean)
+mean(x)
+# obliczenie wariancji dla x1
+var(x)
+# /// /// /// /// /// /// ///
